8ef722c72cb4f83acbc67c424f7d45bdc6480b30
Charles Bartel <charles.bartel@garmin.com>
2012-09-06 13:03:57 -0500

    Initial Commit

---
+++ TestBackgroundDrawing/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  TestBackgroundDrawing
+//
+//  Created by Bartel, Charles on 9/6/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ TestBackgroundDrawing/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TestBackgroundDrawing
+//
+//  Created by Bartel, Charles on 9/6/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
